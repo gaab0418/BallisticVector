7ebf80f08c2523c30666a8b3821fa9d9c85fcf08
gaab0418 <zgabrielw.sz13@gmail.com>
2026-05-21 21:22:07 -0300

    feat: cenas + menu de jogar + warMap

---
+++ scenes/war_map/war_map.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	# 1. Verifica se o evento é um clique de botão do mouse
+	if event is InputEventMouseButton:
+		# 2. Verifica se é o botão esquerdo E se ele acabou de ser pressionado (clique)
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("aaaa")
